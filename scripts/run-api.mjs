631807b32bcaa9a3a3c532c7c447c19272ba7611
manassesnaama <165043038+manassesnaama@users.noreply.github.com>
2026-07-23 08:22:48 -0300

    Update run-api.mjs

--- scripts/run-api.mjs
+++ scripts/run-api.mjs
@@ -1,6 +1,4 @@
 import { ensureSourceExtracted, run } from './common.mjs';
 
 ensureSourceExtracted();
-run('npm', ['run', 'db:deploy', '-w', 'apps/api']);
-run('npm', ['run', 'db:seed:prod', '-w', 'apps/api']);
 run('npm', ['run', 'start', '-w', 'apps/api']);
